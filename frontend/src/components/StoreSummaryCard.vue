160e932778574a8b5cef1d4808dbc9f3e8b191ee
leesk0502 <leesk0502@gmail.com>
2023-03-25 21:37:09 +0900

    [#29] 지도에 상점 요약 카드 정보 노출

---
+++ frontend/src/components/StoreSummaryCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-card no-body class="overflow-hidden" style="max-width: 540px;" :href="url">
+      <b-carousel class="rounded-0"
+          controls
+          indicators
+          :interval="0"
+      >
+        <b-carousel-slide
+          v-for="(item, i) in getThumbnails" :key="i"
+          :img-src="item"
+        >
+        </b-carousel-slide>
+      </b-carousel>
+      <b-card-body :title="name" @click="goUrl">
+        <b-card-sub-title>
+          {{ category }}
+          <b-img :width="20" :height="20" :src="require(`@/assets/images/kakaomap_icon.png`)" v-if="score_kakaomap"></b-img><span>{{ score_kakaomap }}</span>
+          <b-img :width="20" :height="20" :src="require(`@/assets/images/navermap_icon.png`)" v-if="score_navermap"></b-img><span>{{ score_navermap }}</span>
+          <b-img :width="20" :height="20" :src="require(`@/assets/images/googlemap_icon.png`)" v-if="score_googlemap"></b-img><span>{{ score_googlemap }}</span>
+        </b-card-sub-title>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StoreSummaryCard",
+  props: {
+    name: {type: String},
+    category: {type: String},
+    address: {type: String},
+    openingHours: {type: String},
+    score_navermap: {type: String},
+    score_kakaomap: {type: String},
+    score_googlemap: {type: String},
+    thumbnails: {type: String},
+    url: {type: String},
+  },
+  computed: {
+    getThumbnails() { 
+      return this.thumbnails.split(",");
+    }
+  },
+  methods: {
+    goUrl() {
+      if(this.url)
+        window.open(this.url, '_blank');
+    }
+  }
+}
+</script>
+
+<style>
+</style>
