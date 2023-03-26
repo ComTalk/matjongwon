6a430b6751b652bb3f9ce74a7135902c769cba87
leesk0502 <leesk0502@gmail.com>
2023-03-26 19:59:16 +0900

    [#31] cors 설정 변경

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = False
+#CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+        'http://localhost:8000',
+        'http://api.matjongwon.com',
+        'http://matjongwon.com'
+]
