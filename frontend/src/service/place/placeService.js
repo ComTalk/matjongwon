b44d63f7a1375fb763f153bd4304e391e933516b
leesk0502 <leesk0502@gmail.com>
2023-04-19 00:11:03 +0900

    [#34] 리스트 페이지 무한 스크롤 플러그인 적용

--- frontend/src/service/place/placeService.js
+++ frontend/src/service/place/placeService.js
@@ -5,7 +5,6 @@ export default Object.freeze({
     fetchPlaceList(params) {
         return axios.get('/v1/place',{params:params})
             .then(response => {
-                console.log(response.data);
                 return response.data.documents;
             })
             .catch(err => {
