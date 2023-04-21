ecab6776f4ef42a448976224a67ca105d0ee580e
Saron Kim <kasrlmoojoo@gmail.com>
2023-04-21 09:14:22 +0900

    change directory

--- backend/backend/Places/management/commands/loadjson.py
+++ backend/backend/Places/management/commands/loadjson.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from v1.models import Place
+from Places.models import Place
 import json
 import os
 
--- backend/backend/Places/views.py
+++ backend/backend/Places/views.py
@@ -2,18 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.db.models import Q
+from django.core.exceptions import ValidationError
 
 from .models import Place
 import json
 
+def validate_coordinates(coord):
+    try:
+        float(coord)
+    except ValueError:
+        raise ValidationError("Invalid coordinate value.")
+    
 def placeRequest(request):
     # Initialize params
+    
+    if request.method != 'GET':
+        return JsonResponse({"error": "Invalid Request Method"}, status=400)
+    
     query = request.GET.get('query', '강남구')
     page_num = int(request.GET.get('page', 1))
     page_size = int(request.GET.get('size', 10))
+    rect = request.GET.get('rect', None)
 
     # Filer the Place objects by address
     place_objects = Place.objects.filter(Q(address__icontains=query))
+
+    # filter retaurants within the map area
+    if rect:
+        ma
 
     # Caclulate metadata
     total_count = place_objects.count()
