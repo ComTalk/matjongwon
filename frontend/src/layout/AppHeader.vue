b2459190e642d125e98232b7b45c099e22f918f7
Goeon Park <gounttt@gmail.com>
2023-05-07 23:19:22 +0900

    Add layour for social login such as kakao, naver, google

--- frontend/src/layout/AppHeader.vue
+++ frontend/src/layout/AppHeader.vue
@@ -6,58 +6,59 @@
       <b-navbar-brand href="/">맛종원</b-navbar-brand>
 
       <b-navbar-nav class="ml-auto w-50">
-        <b-nav-form class="mw-100" @submit.prevent="">
-          <b-form-input class="mw-100" 
-                        placeholder="검색" 
-                        v-model="keyword"
-                        @keydown="inputKeyDown">
-          </b-form-input>
+        <b-nav-form class="mw-100" @submit.prevent>
+          <b-form-input class="mw-100" placeholder="검색" v-model="keyword" @keydown="inputKeyDown"></b-form-input>
         </b-nav-form>
       </b-navbar-nav>
-      
+
       <b-nav pills class="ml-auto">
-        <b-nav-item size="sm" :active="gnb === 'map'"  @click="clickGnb('map')" href="/">지도</b-nav-item>
+        <b-nav-item size="sm" :active="gnb === 'map'" @click="clickGnb('map')" href="/">지도</b-nav-item>
         <b-nav-item size="sm" :active="gnb === 'list'" @click="clickGnb('list')" href="/list">목록</b-nav-item>
+        <b-nav-item v-b-toggle.sidebar-right>로그인</b-nav-item>
+        <b-sidebar id="sidebar-right" title="소셜로그인" right shadow>
+          <div class="px-3 py-2">
+            <p>
+              로그인을 하시면 원하는 식당을 저장할 수 있어요. 
+            </p>
+            <img src="../assets/images/naver_logo.png" width="60px"></img>
+            <img src="../assets/images/kakao_logo.png" width="60px"></img>
+            <img src="../assets/images/google_logo.png" width="60px"></img>
+          </div>
+        </b-sidebar>
       </b-nav>
 
-      <!--b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">로그인</b-nav-item>
-      </b-navbar-nav-->
-      
       <b-sidebar id="sidebar-1" shadow>
-        <div class="px-3 py-2">
-          
-        </div>
+        <div class="px-3 py-2"></div>
       </b-sidebar>
     </b-navbar>
   </div>
 </template>
 <script>
-
-import {mapMutations, mapState} from 'vuex';
+import { mapMutations, mapState } from "vuex";
 
 export default {
   data() {
     return {
       keyword: ""
-    }
+    };
   },
   computed: {
-    ...mapState(['gnb']),
+    ...mapState(["gnb"])
   },
   methods: {
-    ...mapMutations(['setGnb', 'setPlaceQuery']),
+    ...mapMutations(["setGnb", "setPlaceQuery"]),
 
     clickGnb(gnb) {
       this.setGnb(gnb);
     },
 
     inputKeyDown(e) {
-      if (e.which === 13 || e.keyCode === 13) { // 엔터 키 입력시 검색 처리
+      if (e.which === 13 || e.keyCode === 13) {
+        // 엔터 키 입력시 검색 처리
         this.setPlaceQuery(this.keyword);
       }
-    },
-  },
+    }
+  }
 };
 </script>
 <style>
