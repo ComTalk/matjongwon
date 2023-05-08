d2b09da0636ea91f7a025518a84db249691fadec
hanwool-mango <hanwool@mangomedical.io>
2023-05-08 21:22:07 +0200

    add main login component

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Your other template code here -->
+    <GoogleLogin />
+  </div>
+</template>
+
+<script>
+import GoogleLogin from './GoogleLogin.vue';
+
+export default {
+  components: {
+    GoogleLogin,
+  },
+};
+</script>
