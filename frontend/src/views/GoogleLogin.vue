a0e53458a5d962a6d7779ee6a9291c8c2d9a33bf
hanwool-mango <hanwool@mangomedical.io>
2023-05-08 21:22:26 +0200

    add google login components

---
+++ frontend/src/views/GoogleLogin.vue
@@ -0,0 +1,62 @@
+<template>
+  <div ref="googleLoginBtn" />
+</template>
+
+<script>
+import jwtDecode from "jwt-decode";
+
+export default {
+  mounted() {
+    this.loadGsiLibrary()
+      .then(() => {
+        const gClientId = "client_id";
+        window.google.accounts.id.initialize({
+          client_id: gClientId,
+          callback: this.handleCredentialResponse,
+          auto_select: true,
+        });
+        google.accounts.id.prompt();
+        window.google.accounts.id.renderButton(this.$refs.googleLoginBtn, {
+          text: "signin_with",
+          size: "large",
+          width: "366",
+          theme: "outline",
+          logo_alignment: "left",
+        });
+      })
+      .catch((error) => {
+        console.error("Failed to load Google Identity Services library", error);
+      });
+  },
+  methods: {
+    async loadGsiLibrary() {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.src = "https://accounts.google.com/gsi/client";
+        script.async = true;
+        script.defer = true;
+        script.onload = () => {
+          resolve();
+        };
+        script.onerror = () => {
+          reject(new Error("Failed to load Google Identity Services library"));
+        };
+        document.body.appendChild(script);
+      });
+    },
+    async handleCredentialResponse(response) {
+      // console.log(response.credential);
+      // Put your backend code in here
+
+      const responsePayload = jwtDecode(response.credential);
+
+      console.log("ID: " + responsePayload.sub);
+      console.log('Full Name: ' + responsePayload.name);
+      console.log('Given Name: ' + responsePayload.given_name);
+      console.log('Family Name: ' + responsePayload.family_name);
+      console.log("Image URL: " + responsePayload.picture);
+      console.log("Email: " + responsePayload.email);
+    },
+  },
+};
+</script>
