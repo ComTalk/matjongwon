d29c2d1870acae1819ff50b4afb44fb08397a803
hanwool-mango <hanwool@mangomedical.io>
2023-05-09 18:47:06 +0200

    modified main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from "bootstrap-vue";
 import axios from "axios";
-import store from './store';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import AxiosPlugin from 'vue-axios-cors';
+import AxiosPlugin from "vue-axios-cors";
 
 Vue.use(AxiosPlugin);
 
 axios.defaults.baseURL = process.env.VUE_API_HOST;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
   render: h => h(App),
   store,
   router
-}).$mount('#app')
+}).$mount("#app");
