9eaf2e0feb7ed70985ad4eb7866b7207ad3d99fe
hanwool-mango <hanwool@mangomedical.io>
2023-05-11 13:48:21 +0200

    add search suggestion by using our api

--- frontend/src/layout/AppHeader.vue
+++ frontend/src/layout/AppHeader.vue
@@ -13,6 +13,16 @@
                         @keydown="inputKeyDown">
           </b-form-input>
         </b-nav-form>
+        <div v-if="suggestions.length" class="auto_suggestion">
+        <div v-for="(suggestion, i) in suggestions" 
+            :key="i" 
+            @click="selectSuggestion(suggestion)" 
+            class="auto_suggestion-item">
+          <b-img :src="require('@/assets/images/marker_place_on.png')" class="mr-2 gps-icon" alt="GPS Icon"></b-img>
+          {{ suggestion.name }}
+          <div class="small">{{ suggestion.address }}</div>
+        </div>
+      </div>
       </b-navbar-nav>
       
       <b-nav pills class="ml-auto">
@@ -35,11 +45,13 @@
 <script>
 
 import {mapMutations, mapState} from 'vuex';
+import placeService from '../service/place/placeService';
 
 export default {
   data() {
     return {
-      keyword: ""
+      keyword: "",
+      suggestions: []
     }
   },
   computed: {
@@ -54,10 +66,35 @@ export default {
 
     inputKeyDown(e) {
       if (e.which === 13 || e.keyCode === 13) { // 엔터 키 입력시 검색 처리
-        this.setPlaceQuery(this.keyword);
+        // this.setPlaceQuery(this.keyword);
+        console.log("search word : " + this.keyword)
       }
     },
+    selectSuggestion(suggestion) {
+      this.keyword = suggestion.name;
+      this.suggestions = [];
+    },
   },
+  watch: {
+    keyword(value) {
+    if (value) {
+      let query = {
+        query: value,
+        page: 1
+      }
+
+      placeService.fetchPlaceList(query)
+        .then(documents => {
+          this.suggestions = documents; // use the whole document for the suggestion
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      this.suggestions = [];
+    }
+  }
+  }
 };
 </script>
 <style>
@@ -66,4 +103,38 @@ export default {
     display: none !important;
   }
 }
+
+.auto_suggestion {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  position: absolute;
+  z-index: 99;
+  background-color: white;
+  width: 100%;
+  top: 100%; 
+}
+
+.auto_suggestion-item {
+  padding: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.auto_suggestion-item .small {
+  font-size: 0.8rem;
+  color: #888;
+  margin-left: 24px; 
+}
+
+.gps-icon {
+  width: 16px;
+  height: 16px;
+  object-fit: contain;
+}
+
+.auto_suggestion-item:hover {
+  background-color: #f0f0f0;
+}
+
 </style>
