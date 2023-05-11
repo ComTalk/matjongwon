abf8d4d9bdde9c04a5a1bb70fcd5c76e6c4cc35b
hanwool-mango <hanwool@mangomedical.io>
2023-05-11 14:09:11 +0200

    add explanations in comments

--- frontend/src/layout/AppHeader.vue
+++ frontend/src/layout/AppHeader.vue
@@ -18,8 +18,11 @@
             :key="i" 
             @click="selectSuggestion(suggestion)" 
             class="auto_suggestion-item">
+          <!-- 이미지를 불러와서 GPS 아이콘으로 표시. -->
           <b-img :src="require('@/assets/images/marker_place_on.png')" class="mr-2 gps-icon" alt="GPS Icon"></b-img>
+          <!-- 제안된 장소의 이름을 표시. -->
           {{ suggestion.name }}
+          <!-- 제안된 장소의 주소를 작은 글씨로 표시. -->
           <div class="small">{{ suggestion.address }}</div>
         </div>
       </div>
@@ -51,7 +54,7 @@ export default {
   data() {
     return {
       keyword: "",
-      suggestions: []
+      suggestions: [] // 검색어에 대한 제안 목록을 저장하는 배열
     }
   },
   computed: {
@@ -71,29 +74,38 @@ export default {
       }
     },
     selectSuggestion(suggestion) {
+      // 사용자가 제안된 장소를 선택하면 검색창의 키워드를 해당 장소의 이름으로 설정.
       this.keyword = suggestion.name;
+      // 선택 이후에는 제안 목록을 초기화.
       this.suggestions = [];
     },
   },
   watch: {
+    // 사용자가 검색창에 입력하는 키워드를 관찰.
     keyword(value) {
-    if (value) {
-      let query = {
-        query: value,
-        page: 1
+      // 키워드가 있을 경우,
+      if (value) {
+        // 쿼리를 설정하고,
+        let query = {
+          query: value,
+          page: 1
+        }
+  
+        // 검색 서비스에 쿼리를 보냄.
+        placeService.fetchPlaceList(query)
+          .then(documents => {
+            // 검색 결과를 받아서 제안 목록에 저장.
+            this.suggestions = documents; // 전체 documents를 저장
+          })
+          .catch(err => {
+            // 에러가 발생하면 콘솔에 로그를 출력.
+            console.log(err);
+          });
+      } else {
+        // 키워드가 없을 경우, 제안 목록을 초기화.
+        this.suggestions = [];
       }
-
-      placeService.fetchPlaceList(query)
-        .then(documents => {
-          this.suggestions = documents; // use the whole document for the suggestion
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } else {
-      this.suggestions = [];
     }
-  }
   }
 };
 </script>
@@ -104,35 +116,40 @@ export default {
   }
 }
 
+/* 제안 목록을 포함하는 컨테이너의 스타일을 정의. */
 .auto_suggestion {
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  position: absolute;
-  z-index: 99;
-  background-color: white;
-  width: 100%;
-  top: 100%; 
+  border: 1px solid #ccc; /* 테두리 */
+  border-radius: 4px; /* 모서리 둥글게 */
+  position: absolute; /* 위치 고정 */
+  z-index: 99; /* 다른 요소 위에 위치 */
+  background-color: white; /* 배경색 */
+  width: 100%; /* 너비 */
+  top: 100%;  /* 위치: 검색창 아래 */
 }
 
+/* 각각의 제안 아이템의 스타일을 정의. */
 .auto_suggestion-item {
-  padding: 10px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
+  padding: 10px; /* 패딩 */
+  cursor: pointer; /* 마우스 커서 모양 변경 */
+  display: flex; /* 플렉스박스로 레이아웃 */
+  align-items: center; /* 아이템들을 중앙 정렬 */
 }
 
+/* 제안 아이템의 주소 부분의 스타일을 정의. */
 .auto_suggestion-item .small {
-  font-size: 0.8rem;
-  color: #888;
-  margin-left: 24px; 
+  font-size: 0.8rem; /* 글씨 크기 */
+  color: #888; /* 글씨 색 */
+  margin-left: 24px;  /* 왼쪽 여백 */
 }
 
+/* GPS 아이콘의 크기를 정의. */
 .gps-icon {
-  width: 16px;
-  height: 16px;
-  object-fit: contain;
+  width: 16px; /* 너비 */
+  height: 16px; /* 높이 */
+  object-fit: contain; /* 이미지 크기 조정 */
 }
 
+/* 마우스 오버(호버) 시 제안 아이템의 배경색을 변경. */
 .auto_suggestion-item:hover {
   background-color: #f0f0f0;
 }
